5523ffe30729e6c1edc55eb1ea306803cb4c6aa8
SoramKim <soram523@gmail.com>
2019-05-25 20:38:37 +0900

    second commit

--- src/main/java/edu/handong/analysis/datamodel/Student.java
+++ src/main/java/edu/handong/analysis/datamodel/Student.java
@@ -5,25 +5,32 @@ import java.util.HashMap;
 
 public class Student {
 	private String studentId;
-	private ArrayList<Course> courseTaken;
+	private ArrayList<Course> coursesTaken;
 	private HashMap<String,Integer> semestersByYearAndSemester;
 	
 	public Student(String studentId) {
 		this.studentId= studentId;
-		courseTaken = new ArrayList<Course>();
+		coursesTaken = new ArrayList<Course>();
 		semestersByYearAndSemester = new HashMap<String,Integer>();
 	}
 	public void addCourse(Course newRecord) {
-		courseTaken.add(newRecord);
+		coursesTaken.add(newRecord);
 	}
 	
 	public HashMap<String,Integer> getSemestersByYearAndSemester(){
 		int semester=1;
-		for(Course course : courseTaken) {
+		for(Course course : coursesTaken) {
 			String temp1= Integer.toString(course.getYearTaken());
 			String temp2= Integer.toString(course.getSemesterCourseTaken());
 			String temp3= temp1+"-"+ temp2;
+			
+			if(!semestersByYearAndSemester.containsKey(temp3)) {
+				semestersByYearAndSemester.put(temp3, semester);
+				semester++;
+			}
+			
 		}
+		
 		return semestersByYearAndSemester;
 	}
 	
@@ -32,11 +39,14 @@ public class Student {
 		for(Course course : coursesTaken) {
 			String temp1= Integer.toString(course.getYearTaken());
 			String temp2= Integer.toString(course.getSemesterCourseTaken());
-			String temp3= temp1+ "-" temp2;
+			String temp3= temp1+ "-" +temp2;
 			
-			
+			int tSemester= semestersByYearAndSemester.get(temp3);
+			if(semester== tSemester) {
+				count++;
+			}
 		}
-		return semester;
+		return count;
 	}
 	
 }

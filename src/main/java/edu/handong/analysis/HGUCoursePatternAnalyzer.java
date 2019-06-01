db4bd85cc1adfb6ab30cf5bc74b3cf43cba78f4d
SoramKim <soram523@gmail.com>
2019-06-02 01:04:13 +0900

    new commit

--- src/main/java/edu/handong/analysis/HGUCoursePatternAnalyzer.java
+++ src/main/java/edu/handong/analysis/HGUCoursePatternAnalyzer.java
@@ -8,6 +8,11 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.TreeMap;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 
 import edu.handong.analysis.datamodel.Course;
@@ -100,9 +105,12 @@ public class HGUCoursePatternAnalyzer {
 		String tempStudentId;
 		Student student= null;
 		boolean exist;
+		int tempYear;
 		
-		for(String line : lines) {
-			tempStudentId= line.trim().split(",")[0];
+		for(CSVRecord line : lines) {
+			tempStudentId= line.get(0);
+			tempYear = line.get(7);
+			
 			exist= students.containsKey(tempStudentId);
 			
 			if(exist== false) {
@@ -174,4 +182,99 @@ public class HGUCoursePatternAnalyzer {
 		
 		return linesToBeSaved; // do not forget to return a proper variable.
 	}
+}
+private boolean parseOptions(Options options, String[] args) {
+	CommandLineParser parser = new DefaultParser();
+
+	try {
+
+		CommandLine cmd = parser.parse(options, args);
+		
+		input = cmd.getOptionValue("i");
+		output = cmd.getOptionValue("o");
+		analysis = Integer.parseInt(cmd.getOptionValue("a"));
+		
+		if(analysis == 2) {
+			coursecode = cmd.getOptionValue("c");
+		}
+		
+		startyear = Integer.parseInt(cmd.getOptionValue("s"));
+		endyear = Integer.parseInt(cmd.getOptionValue("e"));
+		help = cmd.hasOption("h");
+		
+	
+	} catch (Exception e) {
+		printHelp(options);
+		return false;
+	}
+
+	return true;
+}
+
+// Definition Stage
+private Options createOptions() {
+	Options options = new Options();
+
+	// add options by using OptionBuilder
+	options.addOption(Option.builder("i").longOpt("input")
+			.desc("Set an input file path")
+			.hasArg()
+			.argName("Input path")
+			.required()
+			.build());
+
+	// add options by using OptionBuilder
+	options.addOption(Option.builder("o").longOpt("output")
+			.desc("Set an output file path")
+			.hasArg()     
+			.argName("Output path")
+			.required() 
+			.build());
+	
+	// add options by using OptionBuilder
+	options.addOption(Option.builder("a").longOpt("analysis")
+			.desc("1: Count courses per semester, 2: Count per course name and year")
+			.hasArg()
+			.argName("Analysis option")
+			.required()
+			.build());
+	
+	// add options by using OptionBuilder
+	options.addOption(Option.builder("c").longOpt("coursecode")
+			.desc("Course code for '-a 2' option")
+			.hasArg()
+			.argName("course code")
+			.required((analysis == 2))
+			.build());
+	
+	// add options by using OptionBuilder
+	options.addOption(Option.builder("s").longOpt("startyear")
+			.desc("Set the start year for analysis e.g., -s 2002")
+			.hasArg()
+			.argName("Start year for analysis")
+			.required()
+			.build());		
+
+	// add options by using OptionBuilder
+	options.addOption(Option.builder("e").longOpt("endyear")
+			.desc("Set the end year for analysis e.g., -e 2005")
+			.hasArg()
+			.argName("End year for analysis")
+			.required()
+			.build());
+	
+	// add options by using OptionBuilder
+	options.addOption(Option.builder("h").longOpt("help")
+	        .desc("Show a Help page")
+	        .argName("Help")
+	        .build());
+	
+	return options;
+}
+private void printHelp(Options options) {
+	// automatically generate the help statement
+	HelpFormatter formatter = new HelpFormatter();
+	String header = "HGU Course Analyzer";
+	String footer ="";
+	formatter.printHelp("HGUCourseCounter", header, options, footer, true);
 }
